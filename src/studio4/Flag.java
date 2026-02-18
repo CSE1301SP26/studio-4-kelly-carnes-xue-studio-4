0e0d59122fe93da63022dab8021088eb08c90a90
mxue926 <x.yizhou@wustl.edu>
2026-02-18 15:45:08 -0600

    Studio 4 done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,20 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.clear();
+		StdDraw.setPenColor(0, 0, 0);
+		StdDraw.filledRectangle(0.5, 0.5, 6, 3);
+		StdDraw.setPenColor(255, 255, 255);
+		StdDraw.filledCircle(0.5, 0.7, 0.2);
+		StdDraw.setPenColor(0, 0, 0);
+		StdDraw.filledCircle(0.4, 0.7, 0.02);
+		StdDraw.filledCircle(0.6, 0.7, 0.02);
+		StdDraw.setPenRadius(0.005);
+		StdDraw.arc(0.45, 0.4, 0.01, 0, 90);
+		StdDraw.setPenRadius(0.05);
+		StdDraw.setPenColor(255, 255, 255);
+		StdDraw.line(0.2, 0.2, 0.8, 0.5);
+		StdDraw.line(0.2, 0.5, 0.8, 0.2);
+
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,26 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shapeType = in.next(); 
+		int red = in.nextInt(); 
+		int green = in.nextInt(); 
+		int blue = in.nextInt(); 
+		boolean isFilled = in.nextBoolean(); 
+		double x = in.nextDouble(); 
+		double y = in.nextDouble(); 
+		double halfW = in.nextDouble(); 
+		double halfH = in.nextDouble(); 
+		StdDraw.setPenColor(red, green, blue); 
+		StdDraw.setPenRadius(0.02); 
+		if (shapeType.equals("ellipse") && isFilled) {
+		StdDraw.filledEllipse(x, y, halfW, halfH); 
+		} else if (shapeType.equals("ellipse") && !isFilled) {
+		StdDraw.ellipse(x, y, halfW, halfH);
+		} else if (shapeType.equals("rectangle") && isFilled) {
+		StdDraw.filledRectangle(x, y, halfW, halfH);  
+		} else if (shapeType.equals("rectangle") && !isFilled) {
+			StdDraw.rectangle(x, y, halfW, halfH);
+		} 
 		
 	}
 }
